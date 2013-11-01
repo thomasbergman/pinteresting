ee1011d85303f130e6ff12ecaa4ba72521f3e113
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-11-02 00:38:50 +0100

    added S3

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
 gem 'devise', '~> 3.1.1'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk', '~> 1.24.0'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pinteresting::Application.configure do
   #Required for Devise
   #Set this to the actual host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  #Sets Paperclip to upload to AWS S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
